5f34ae00302d870a61a570a9c1ede6c16be8dd8d
FzrFernando <74764616+FzrFernando@users.noreply.github.com>
2023-10-09 21:18:23 +0200

    Terminar

--- Ejercicio.js
+++ Ejercicio.js
@@ -52,3 +52,33 @@ primerElemento.replaceWith(nuevoElemento);
 
 // 3.4. Utiliza el método nuevoElemento.remove() para eliminar el nuevoElemento de la página.
 nuevoElemento.remove();
+
+
+// Ejercicio 4: Atributos y Estilos
+// 4.1. Utiliza document.querySelector() para obtener un elemento de tu página web que tenga un atributo id. Almacena este elemento en una variable.
+let variableId = document.querySelector('[id]');
+
+// 4.2. Usa el método elemento.setAttribute('nombreAtributo', 'valor') para agregar un atributo "data-nuevo" con el valor "123" al elemento obtenido en el ejercicio 4.1.
+variableId.setAttribute('data-nuevo','123');
+
+// 4.3. Utiliza el método elemento.removeAttribute('nombreAtributo') para eliminar el atributo "data-nuevo" del elemento.
+variableId.removeAttribute('data-nuevo');
+
+// 4.4. Cambia el color de fondo de un elemento de tu página web utilizando la propiedad style.backgroundColor. Asegúrate de que el cambio de estilo se realice mediante JavaScript.
+let elementoEstilo = document.querySelector('.important');
+elementoEstilo.computedStyleMap.backgroundColor = 'red';
+
+
+// Ejercicio 5: Clases y ClassList
+// 5.1. Utiliza document.querySelector() para obtener un elemento de tu página web y asigna una clase "destacado" a este elemento utilizando la propiedad elemento.className.
+let elementoDestacado = document.querySelector('p');
+elementoDestacado.className = 'destacado';
+
+// 5.2. Utiliza la propiedad classList para agregar la clase "nuevo-estilo" al elemento obtenido en el ejercicio 5.1.
+elementoDestacado.classList.add('nuevo-estilo');
+
+// 5.3. Emplea la propiedad classList para eliminar la clase "destacado" del elemento obtenido en el ejercicio 5.1.
+elementoDestacado.classList.remove('destacado');
+
+// 5.4. Comprueba si el elemento obtenido en el ejercicio 5.1 tiene la clase "nuevo-estilo" utilizando el método classList.contains().
+elementoDestacado.classList.contains('nuevo-estilo');
